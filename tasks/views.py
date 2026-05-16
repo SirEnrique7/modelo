6d3669b0f5386717159ceebb901c93aceb2c9ac1
jesus <jesusenrique13721@gmail.com>
2026-05-16 17:31:36 -0400

    tasks detail is done

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Task
 # Create your views here.
 
 class TaskListView(ListView):
     model = Task
     template_name = 'tasks_list.html'
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task-detail.html'
 
